570c38b1dfd9fcd3b4dfffd60bf50811fa663679
Guilherme Alves <guilherme.alves3340@gmail.com>
2019-02-02 00:57:02 -0200

    upload metodo post

--- backend/upload.py
+++ backend/upload.py
@@ -1,12 +1,11 @@
 from flask_restful import Resource, reqparse
-from flask import request, jsonify
+from flask import request, jsonify, send_from_directory, make_response
 import werkzeug
 from datetime import datetime
 import random as rd
 from hashlib import md5
 #from request_file import define_file, save_path_file
-
-modules = "a b c d e f g h i  k ".split()
+import codecs
 
 
 class Upload(Resource):
@@ -27,16 +26,11 @@ class Upload(Resource):
         }
 
     def get(self):
+        modules = "a b c d e f g h i  k ".split()
         data = request.get_json(force=True)
-
-        
-
-        return open('guiFormal.jpeg')
-
-        """# find data['_id']
+        find data['_id']
         db = Database('users')
         user = db.find_one({'_id': ObjectId(data['_id'])})
-
         if data['update_module']:
             # insert em videos_ileel com o data['path_file'] #OK
             # update em users.dataset #OK
@@ -53,23 +47,32 @@ class Upload(Resource):
                 'current_module': user['dataset']['current_module']
             }
             user.update_one(update,user)
-
         # verifica a contagem dos videos    #TODO
         # atualiza a contagem e modulo(se necessario) #TODO
-
         if user['dataset']['#video'] == 50:
             if user['dataset']['#video'] != modules[-1]:
                 _module = modules[modules.index(user['dataset']['current_module']) +1]
 
                 # busca a url do video #TODO
-                url = find_file(_module)
+                url = self.find_file(_module)
                 # envia o video pro front #TODO
-
+                response = send_from_directory(url['diretory'],url['file_name'])
+                # editar headers #TODO
+                return response
             else:
                 return {'status':'finally'}
+        else:
+            _module = modules[modules.index(user['dataset']['current_module'])+1]
+            url = find_file(_module)
+            response = send_from_directory(url['diretory'],url['file_name'])
+            return response
 
 
-
-    def find_file(module):
+    def find_file(self,module):
         pass
-"""
+
+
+
+#response = send_from_directory('./', 'pp.jpeg')
+#response.headers['my-custom-header'] = 'Este sou eu Guilherme Alves'
+#return response
